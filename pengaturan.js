a6d5ad08c3adf92171fbb4498c762ced66bffa28
loviaphoto14-max <loviaphoto14@gmail.com>
2026-07-22 11:26:00 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -1,10 +1,6 @@
-// PENGATURAN APLIKASI CLIENT PORTAL[cite: 1]
+// PENGATURAN APLIKASI CLIENT PORTAL
 const CONFIG_APP = {
-    namaVendor: "Portal Client",[cite: 1]
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",[cite: 1]
-    whatsappAdmin: "6282117102831",[cite: 1]
-
-    // Konfigurasi Supabase
-    supabaseUrl: "https://ihqafpnqmlhewwyatiau.supabase.co/rest/v1/",
-    supabaseKey: "sb_publishable_ClY-tvZnSpQqrpG2I9a2fA_q6cRhAdC"
+    namaVendor: "Portal Client",
+    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
+    whatsappAdmin: "6282117102831",
 };
